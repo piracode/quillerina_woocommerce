63794492c8d673dea9fd4be5c8dd20489a59a06a
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-07-15 19:23:20 -0700

    enqueue font styles, add footer menu

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,26 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'quillerina-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'quillerina-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'quillerina-theme' ), 'quillerina-theme', '<a href="http://underscores.me/">Martha Villa Martin</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<nav id="site-navigation" class="footer-navigation">
+		<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php esc_html_e('Footer Menu', 'quillerina-theme'); ?></button>
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'footer-menu',
+				'menu_id'        => 'footer-menu',
+			)
+		);
+		?>
+	</nav><!-- #site-navigation -->
+	<div class="site-info">
+		<p>&copy; <?php echo date('Y'); ?> <?php echo esc_html__('Quillerina Paper Art 2023', 'your-text-domain'); ?></p>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
