4c4aa1d1514f3e912b996a6e91351aac816ec293
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-07-16 17:19:01 -0700

    Fetch CTA fields description art

--- front-page.php
+++ front-page.php
@@ -18,44 +18,224 @@ get_header();
 		<section class="section-hero">
 			<?php
 			if (function_exists('get_field')) {
+
 				$hero_heading = get_field('hero_section')['hero_heading'];
 				if ($hero_heading) {
 					echo '<h1>' . $hero_heading . '</h1>';
 				}
-				$welcome_message = get_field('hero_section')['welcome_message'];
 
+				$welcome_message = get_field('hero_section')['welcome_message'];
 				if ($welcome_message) {
 					echo '<p>' . $welcome_message . '</p>';
 				}
+			?>
+				<div class="cta-container">
 
-				$shop_now_cta = get_field('hero_section')['shop_now_cta'];
+					<?php
+
+					$shop_now_cta = get_field('hero_section')['shop_now_cta'];
+					if ($shop_now_cta) {
+						$shop_now_cta_url = $shop_now_cta['url'];
+						$shop_now_cta_title = $shop_now_cta['title'];
+						// var_dump($shop_now_cta_title);
+						echo '<a class="shop-now-cta button" href="' . $shop_now_cta_url . '">' . $shop_now_cta_title . '</a>';
+					}
+					$custom_orders_cta = get_field('hero_section')['custom_orders_cta'];
+					if ($custom_orders_cta) {
+						$custom_orders_cta_url = $custom_orders_cta['url'];
+						$custom_orders_cta_title = $custom_orders_cta['title'];
+						echo '<a class="custom-orders-cta button" href="' . $custom_orders_cta_url . '">' . $custom_orders_cta_title . '</a>';
+					}
+					?>
+				</div>
+
+			<?php
+			}
+
+			// echo do_shortcode('[metaslider id="685"]');
+
+
+			?>
+		</section>
+
+		<section class="description-art-section">
+			<?php
+			if (function_exists('get_field')) {
+
+				//Pet Portraits Description
+
+				$pet_portrait_featured_image = get_field('description_section')['pet_portrait_featured_image'];
+				if ($pet_portrait_featured_image && isset($pet_portrait_featured_image['url'])) {
+					echo '<img src="' . esc_url($pet_portrait_featured_image['url']) . '" alt="Pet Portrait Featured Image">';
+				}
+
+				$pet_portrait_heading = get_field('description_section')['pet_portrait_heading'];
+				if ($pet_portrait_heading) {
+					echo '<h2>' . $pet_portrait_heading . '</h2>';
+				}
+
+				$pet_portrait_text = get_field('description_section')['pet_portrait_text'];
+				if ($pet_portrait_text) {
+					echo '<p>' . $pet_portrait_text . '</p>';
+				}
+
+
+				//Earrings Description
+				$earrings_featured_image = get_field('description_section')['earrings_featured_image'];
+				if ($earrings_featured_image && isset($earrings_featured_image['url'])) {
+					echo '<img src="' . esc_url($earrings_featured_image['url']) . '" alt="Earrings Featured Image">';
+				}
+
+				$earrings_heading = get_field('description_section')['earrings_heading'];
+				if ($earrings_heading) {
+					echo '<h2>' . $earrings_heading . '</h2>';
+				}
+
+				$earrings_text = get_field('description_section')['earrings_text'];
+				if ($earrings_text) {
+					echo '<p>' . $earrings_text . '</p>';
+				}
+
+				//Other Artowrk Description
+
+				$artwork_featured_image = get_field('description_section')['artwork_featured_image'];
+				if ($artwork_featured_image && isset($artwork_featured_image['url'])) {
+					echo '<img src="' . esc_url($artwork_featured_image['url']) . '" alt="Artwork Featured Image">';
+				}
+
+				$artwork_heading = get_field('description_section')['artwork_heading'];
+				if ($artwork_heading) {
+					echo '<h2>' . $artwork_heading . '</h2>';
+				}
+
+				//Landscapes
+
+			?>
+				<div class="description-art-title-box">
+
+
+					<?php
+					$landscape_svg = get_field('description_section')['landscape_svg'];
+					if ($landscape_svg) {
+						echo '<div>' . $landscape_svg . '</div>';
+					}
+
+					$landscape_subheading = get_field('description_section')['landscape_subheading'];
+					if ($landscape_subheading) {
+						echo '<h3 class="art-description-subheading">' . $landscape_subheading . '</h3>';
+					}
+
+					?>
+				</div>
+				<?php
+
+
+				$landscape_text = get_field('description_section')['landscape_text'];
+				if ($landscape_text) {
+					echo '<p>' . $landscape_text . '</p>';
+				}
+
+				//Floral
+
+				?>
+				<div class="description-art-title-box">
+
+
+					<?php
+					$floral_svg = get_field('description_section')['floral_svg'];
+					if ($floral_svg) {
+						echo '<div>' . $floral_svg . '</div>';
+					}
+
+					$floral_art_subheading = get_field('description_section')['floral_art_subheading'];
+					if ($floral_art_subheading) {
+						echo '<h3 class="art-description-subheading">' . $floral_art_subheading . '</h3>';
+					}
+
+					?>
+				</div>
+				<?php
+
+				$floral_art_text = get_field('description_section')['floral_art_text'];
+				if ($floral_art_text) {
+					echo '<p>' . $floral_art_text . '</p>';
+				}
+
+				//Ornaments
+
+				?>
+				<div class="description-art-title-box">
+
+
+					<?php
+
+					$ornaments_svg = get_field('description_section')['ornaments_svg'];
+					if ($ornaments_svg) {
+						echo '<div>' . $ornaments_svg . '</div>';
+					}
+
+					$ornaments_subheading = get_field('description_section')['ornaments_subheading'];
+					if ($ornaments_subheading) {
+						echo '<h3 class="art-description-subheading">' . $ornaments_subheading . '</h3>';
+					}
+
+					?>
+				</div>
+				<?php
+				$ornaments_text = get_field('description_section')['ornaments_text'];
+				if ($ornaments_text) {
+					echo '<p>' . $ornaments_text . '</p>';
+				}
+
+				//Commissions
+
+				?>
+				<div class="description-art-title-box">
+
+
+					<?php
+
+					$commissions_svg = get_field('description_section')['commissions_svg'];
+					if ($commissions_svg) {
+						echo '<div>' . $commissions_svg . '</div>';
+					}
+
+					$commissions_subheading = get_field('description_section')['commissions_subheading'];
+					if ($commissions_subheading) {
+						echo '<h3 class="art-description-subheading">' . $commissions_subheading . '</h3>';
+					}
+
+					?>
+				</div>
+				<?php
+
+				$commissions_text = get_field('description_section')['commisssions_text'];
+				if ($commissions_text) {
+					echo '<p>' . $commissions_text . '</p>';
+				}
+				?>
+
+				<div class="cta-container">
+
+				<?php
+
+				$shop_now_cta = get_field('description_section')['shop_now_cta'];
 				if ($shop_now_cta) {
 					$shop_now_cta_url = $shop_now_cta['url'];
 					$shop_now_cta_title = $shop_now_cta['title'];
 					// var_dump($shop_now_cta_title);
-					echo '<a class="hero-cta" href="' . $shop_now_cta_url . '">' . $shop_now_cta_title . '</a>';
+					echo '<a class="shop-now-cta button" href="' . $shop_now_cta_url . '">' . $shop_now_cta_title . '</a>';
 				}
-				$custom_orders_cta = get_field('hero_section')['custom_orders_cta'];
+				$custom_orders_cta = get_field('description_section')['custom_order_cta'];
 				if ($custom_orders_cta) {
 					$custom_orders_cta_url = $custom_orders_cta['url'];
 					$custom_orders_cta_title = $custom_orders_cta['title'];
-					echo '<a class="hero-cta" href="' . $custom_orders_cta_url . '">' . $custom_orders_cta_title . '</a>';
-				}
-
-				if (function_exists('get_field')) {
-					echo '<div class="gallery">';
-					for ($i = 1; $i <= 6; $i++) {
-						$image = get_field('hero_section')['hero_image_gallery_' . $i];
-						if ($image) {
-							echo '<figure class="gallery-item">';
-							echo '<img src="' . esc_url($image['url']) . '" alt="' . esc_attr($image['alt']) . '">';
-							echo '</figure>';
-						}
-					}
-					echo '</div>';
+					echo '<a class="custom-orders-cta button" href="' . $custom_orders_cta_url . '">' . $custom_orders_cta_title . '</a>';
 				}
 			}
-			?>
+				?>
+				</div>
+
 		</section>
 
 		<section class="featured-artwork">
@@ -109,15 +289,16 @@ get_header();
 
 			<?php
 			if (function_exists('get_field')) {
-				$faq_section = get_field('faq_section'); // Retrieve the faq_section field
+				$faq_section = get_field('faq_section');
 
 				$faq_heading = $faq_section['faq_heading'];
 				if ($faq_heading) {
 					echo '<h2>' . $faq_heading . '</h2>';
 				}
 
-				// Loop through the questions using a for loop
 				for ($i = 1; $i <= 5; $i++) {
+					echo '<div class="faq-heading-box faq-box-' . $i . '">'; // Start div container
+
 					$question_svg = $faq_section['question' . $i . '_svg'];
 					if ($question_svg) {
 						echo $question_svg;
@@ -128,6 +309,8 @@ get_header();
 						echo '<h3>' . $question_heading . '</h3>';
 					}
 
+					echo '</div>'; // End div container
+
 					$question_text = $faq_section['question' . $i . '_text'];
 					if ($question_text) {
 						echo '<p>' . $question_text . '</p>';
@@ -135,11 +318,40 @@ get_header();
 				}
 			}
 			?>
+			<?php
+
+			if (function_exists('get_field')) {
+
+				$hero_heading = get_field('hero_section')['hero_heading'];
+			?>
+				<div class="cta-container">
+
+					<?php
+
+					$shop_now_cta = get_field('hero_section')['shop_now_cta'];
+					if ($shop_now_cta) {
+						$shop_now_cta_url = $shop_now_cta['url'];
+						$shop_now_cta_title = $shop_now_cta['title'];
+						// var_dump($shop_now_cta_title);
+						echo '<a class="shop-now-cta button" href="' . $shop_now_cta_url . '">' . $shop_now_cta_title . '</a>';
+					}
+					$custom_orders_cta = get_field('hero_section')['custom_orders_cta'];
+					if ($custom_orders_cta) {
+						$custom_orders_cta_url = $custom_orders_cta['url'];
+						$custom_orders_cta_title = $custom_orders_cta['title'];
+						echo '<a class="custom-orders-cta button" href="' . $custom_orders_cta_url . '">' . $custom_orders_cta_title . '</a>';
+					}
+					?>
+				</div>
 		</section>
 
+<?php
+			}
 
 
-	<?php endwhile; ?>
+
+
+		endwhile; ?>
 </main>
 
 <?php
