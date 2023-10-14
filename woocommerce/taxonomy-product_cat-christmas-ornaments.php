069e1a4c8cc8d0b9a0b5c529d4406217087f2a29
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-10-14 11:45:02 -0700

    Fix christmas ornament php file

--- woocommerce/taxonomy-product_cat-christmas-ornaments.php
+++ woocommerce/taxonomy-product_cat-christmas-ornaments.php
@@ -8,14 +8,39 @@
 defined('ABSPATH') || exit;
 
 get_header('shop');
-
+echo '<section class="category-products-section">';
 if (have_posts()) {
     while (have_posts()) {
         the_post();
-        wc_get_template_part('content', 'product');
+        global $product;
+
+        // Wrap the eqch product in an anchor tag and an article tag
+        echo '<article class="product-container">';
+        echo '<a href="' . esc_url($product->get_permalink()) . '">';
+
+        // Display product title
+        echo '<h2>' . get_the_title() . '</h2>';
+
+        if (has_post_thumbnail()) {
+            // Display the product thumbnail
+            the_post_thumbnail('custom-thumbnail');
+        }
+
+        // Display product price
+        echo '<p>' . $product->get_price_html() . '</p>';
+
+        // Display "Select options" button
+        if ($product->is_type('variable')) {
+            echo '<button class="option-button">' . esc_html__('See available colors', 'woocommerce') . '</button>';
+        }
+
+        echo '</a>';
+        echo '</article>';
     }
 } else {
     echo 'No products found.';
 }
+
+echo '</section>';
 
 get_footer('shop');
